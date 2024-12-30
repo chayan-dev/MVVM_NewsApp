e729b1f2ef2db9ab7d457c080514e09675044ea6
chayan <chayandas030@gmail.com>
2024-12-30 12:52:41 +0530

    key included

--- app/src/main/java/com/example/mvvm_newsapp/util/Constants.kt
+++ app/src/main/java/com/example/mvvm_newsapp/util/Constants.kt
@@ -3,6 +3,7 @@ package com.example.mvvm_newsapp.util;
 class Constants {
     companion object{
         const val API_KEY="3155be924dad444eb5bdfe0206c5afb1"
+//        const val API_KEY="a36a216745034702aa31480835db4ce3"
         const val BASE_URL="https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY=500L
         const val PAGE_SIZE=10
